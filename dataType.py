f1a622d432c6d51deb6ff0a2a0f844e888bf015d
zbahati <bahatizirimwabagabo@gmail.com>
2024-01-03 07:57:21 +0200

    Add first exerces where we receive input and return the Message Greating

---
+++ dataType.py
@@ -0,0 +1,7 @@
+name = 'John smith'
+age = 20
+is_new_patient = False
+
+print(name)
+print(age)
+print(is_new_patient)
---
+++ exercise.py
@@ -0,0 +1,6 @@
+name = input('What is your name?:  ')
+age = input('Enter your age: ')
+
+print("_" * 20)
+print('Hello ' + name)
+print('You are ' + age + ' Year Old')
