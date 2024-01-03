b69aadaff100612c6e9ac8798d40750bfdd137da
zbahati <bahatizirimwabagabo@gmail.com>
2024-01-03 18:44:05 +0200

    Add lists and for loop iterators

---
+++ loop.py
@@ -0,0 +1,17 @@
+# while loop
+
+# i = 3
+# while i != 0:
+#   print(f"Go")
+#   i -=1
+
+
+# For loop
+
+
+for i in [0,1,2,3] :
+  print('miow')
+
+name = ['Bahati', 'John', 'merry']
+for i in range(len(name)):
+  print(i)
